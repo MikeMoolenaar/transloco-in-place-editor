372ef4f3f217d272f39a309bbde81a39b764f8b8
MikeMoolenaar <heeylego@gmail.com>
2022-10-04 23:07:52 +0200

    Remove js files

--- WebApp/webpack-config/custom-webpack-loader.ts
+++ WebApp/webpack-config/custom-webpack-loader.ts
@@ -2,11 +2,17 @@ const insertAt = (str: string, pos: number, value: string) => {
   return str.substring(0, pos) + value + str.substring(pos);
 }
 
+const regex = />\s*{{\s*t\('([\w.]+)'\).*}}/i;
 const loader = (content: string) => {
-  const index = content.indexOf('{{ t(')
-  if (index) {
-    console.log('Bingo!');
-    content = insertAt(content,index-1, ' data-transloco-key="hey!"');
+  let match;
+  while ((match = regex.exec(content)) !== null) {
+    if (match.index === regex.lastIndex)
+      regex.lastIndex++;
+
+    const key = match.groups?.[0];
+    if (!key) continue;
+
+    content = insertAt(content, match.index, ` data-transloco-key="${key}"`)
   }
   console.log(content);
 
